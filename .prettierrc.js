9e69ec95779d613a4d88977a5f4fe765f65b32e7
sw8p <khorysa@gmail.com>
2018-10-16 17:03:31 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'avoid',
+}
