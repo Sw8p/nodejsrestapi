cc97a1024976ea94dc1b4bed51b088310b48a6c7
sw8p <khorysa@gmail.com>
2018-10-17 15:57:58 +0200

    init config project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+module.exports = {
+    mode: 'development',
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    externals: [nodeExternals()],
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        libraryTarget: 'commonjs2',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+}
